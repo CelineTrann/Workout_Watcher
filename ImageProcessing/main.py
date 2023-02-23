72c0c380b08f3b5c1f710a1a0bfdafb76979db36
Celine Tran <celinephoungtran@gmail.com>
2023-02-23 12:45:15 -0500

    save data label instead of file path

--- ImageProcessing/main.py
+++ ImageProcessing/main.py
@@ -6,7 +6,7 @@ import cv2 as cv
 import process_data as pd
 from read import read_file, show_img
 
-def main(path, display_img=False):
+def main(path, display_img=False, data_label="foot", output="test1.csv"):
     box_data_list = []
 
     for base_file, data_file in zip(base_files, data_files):
@@ -27,7 +27,7 @@ def main(path, display_img=False):
 
         for rect in rects:
             # Save Data
-            box_data = pd.Bounding_Box(data_filepath, rect)
+            box_data = pd.Bounding_Box(data_filepath, rect, data_label)
             box_data_list.append(box_data)
 
             # Rotate and Crop Image
@@ -39,71 +39,72 @@ def main(path, display_img=False):
         print(f"{data_filepath} - {number} objects")
         
         
-    pd.create_data_csv(box_data_list, "test.csv")
+    pd.create_data_csv(box_data_list, output)
 
+# ----------------------------------------------------------------------------------------------------------------------------
 
 # Left Foot
-# base_files = ['baself10.txt', 'baself11.txt', 'baself12.txt', 'baself13.txt', 'baself14.txt', 
-#               'baself15.txt', 'baself16.txt', 'baself17.txt', 'baself18.txt', 'baself19.txt', 
-#               'baself1.txt', 'baself20.txt', 'baself21.txt', 'baself22.txt', 'baself23.txt', 
-#               'baself24.txt', 'baself25.txt', 'baself26.txt', 'baself27.txt', 'baself28.txt', 
-#               'baself29.txt', 'baself2.txt', 'baself30.txt', 'baself31.txt', 'baself32.txt', 
-#               'baself33.txt', 'baself34.txt', 'baself35.txt', 'baself36.txt', 'baself3.txt', 
-#               'baself4.txt', 'baself5.txt', 'baself6.txt', 'baself7.txt', 'baself8.txt', 'baself9.txt']
+base_files = ['baself10.txt', 'baself11.txt', 'baself12.txt', 'baself13.txt', 'baself14.txt', 
+              'baself15.txt', 'baself16.txt', 'baself17.txt', 'baself18.txt', 'baself19.txt', 
+              'baself1.txt', 'baself20.txt', 'baself21.txt', 'baself22.txt', 'baself23.txt', 
+              'baself24.txt', 'baself25.txt', 'baself26.txt', 'baself27.txt', 'baself28.txt', 
+              'baself29.txt', 'baself2.txt', 'baself30.txt', 'baself31.txt', 'baself32.txt', 
+              'baself33.txt', 'baself34.txt', 'baself35.txt', 'baself36.txt', 'baself3.txt', 
+              'baself4.txt', 'baself5.txt', 'baself6.txt', 'baself7.txt', 'baself8.txt', 'baself9.txt']
 
-# data_files = ['leftf10_par.txt', 'leftf11_par.txt', 'leftf12_par.txt', 'leftf13_per.txt', 'leftf14_per.txt', 
-#               'leftf15_per.txt', 'leftf16_per.txt', 'leftf17_per.txt', 'leftf18_per.txt', 'leftf19_per.txt', 
-#               'leftf1_par.txt', 'leftf20_per.txt', 'leftf21_per.txt', 'leftf22_per.txt', 'leftf23_per.txt', 
-#               'leftf24_per.txt', 'leftf25_ang.txt', 'leftf26_ang.txt', 'leftf27_ang.txt', 'leftf28_ang.txt', 
-#               'leftf29_ang.txt', 'leftf2_par.txt', 'leftf30_ang.txt', 'leftf31_ang.txt', 'leftf32_ang.txt', 
-#               'leftf33_ang.txt', 'leftf34_ang.txt', 'leftf35_ang.txt', 'leftf36_ang.txt', 'leftf3_par.txt', 
-#               'leftf4_par.txt', 'leftf5_par.txt', 'leftf6_par.txt', 'leftf7_par.txt', 'leftf8_par.txt', 'leftf9_par.txt']
+data_files = ['leftf10_par.txt', 'leftf11_par.txt', 'leftf12_par.txt', 'leftf13_per.txt', 'leftf14_per.txt', 
+              'leftf15_per.txt', 'leftf16_per.txt', 'leftf17_per.txt', 'leftf18_per.txt', 'leftf19_per.txt', 
+              'leftf1_par.txt', 'leftf20_per.txt', 'leftf21_per.txt', 'leftf22_per.txt', 'leftf23_per.txt', 
+              'leftf24_per.txt', 'leftf25_ang.txt', 'leftf26_ang.txt', 'leftf27_ang.txt', 'leftf28_ang.txt', 
+              'leftf29_ang.txt', 'leftf2_par.txt', 'leftf30_ang.txt', 'leftf31_ang.txt', 'leftf32_ang.txt', 
+              'leftf33_ang.txt', 'leftf34_ang.txt', 'leftf35_ang.txt', 'leftf36_ang.txt', 'leftf3_par.txt', 
+              'leftf4_par.txt', 'leftf5_par.txt', 'leftf6_par.txt', 'leftf7_par.txt', 'leftf8_par.txt', 'leftf9_par.txt']
 
-# main("Data\Data (02.21)\Left Foot", False)
+main("Data\Data (02.21)\Left Foot", False, 'foot')
 
-## ----------------------------------------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------------------------------------
 
 # Right Foot
-# base_files = ['baserf10.txt', 'baserf11.txt', 'baserf12.txt', 'baserf13.txt', 'baserf14.txt', 
-#               'baserf15.txt', 'baserf16.txt', 'baserf17.txt', 'baserf18.txt', 'baserf19.txt', 
-#               'baserf1.txt', 'baserf20.txt', 'baserf21.txt', 'baserf22.txt', 'baserf23.txt', 
-#               'baserf24.txt', 'baserf25.txt', 'baserf26.txt', 'baserf27.txt', 'baserf28.txt', 
-#               'baserf29.txt', 'baserf2.txt', 'baserf30.txt', 'baserf31.txt', 'baserf32.txt', 
-#               'baserf33.txt', 'baserf34.txt', 'baserf35.txt', 'baserf36.txt', 'baserf3.txt', 
-#               'baserf4.txt', 'baserf5.txt', 'baserf6.txt', 'baserf7.txt', 'baserf8.txt', 'baserf9.txt']
+base_files = ['baserf10.txt', 'baserf11.txt', 'baserf12.txt', 'baserf13.txt', 'baserf14.txt', 
+              'baserf15.txt', 'baserf16.txt', 'baserf17.txt', 'baserf18.txt', 'baserf19.txt', 
+              'baserf1.txt', 'baserf20.txt', 'baserf21.txt', 'baserf22.txt', 'baserf23.txt', 
+              'baserf24.txt', 'baserf25.txt', 'baserf26.txt', 'baserf27.txt', 'baserf28.txt', 
+              'baserf29.txt', 'baserf2.txt', 'baserf30.txt', 'baserf31.txt', 'baserf32.txt', 
+              'baserf33.txt', 'baserf34.txt', 'baserf35.txt', 'baserf36.txt', 'baserf3.txt', 
+              'baserf4.txt', 'baserf5.txt', 'baserf6.txt', 'baserf7.txt', 'baserf8.txt', 'baserf9.txt']
 
-# data_files = ['rightf10_par.txt', 'rightf11_par.txt', 'rightf12_par.txt', 'rightf13_per.txt', 'rightf14_per.txt', 
-#               'rightf15_per.txt', 'rightf16_per.txt', 'rightf17_per.txt', 'rightf18_per.txt', 'rightf19_per.txt', 
-#               'rightf1_par.txt', 'rightf20_per.txt', 'rightf21_per.txt', 'rightf22_per.txt', 'rightf23_per.txt', 
-#               'rightf24_per.txt', 'rightf25_ang.txt', 'rightf26_ang.txt', 'rightf27_ang.txt', 'rightf28_ang.txt', 
-#               'rightf29_ang.txt', 'rightf2_par.txt', 'rightf30_ang.txt', 'rightf31_ang.txt', 'rightf32_ang.txt', 
-#               'rightf33_ang.txt', 'rightf34_ang.txt', 'rightf35_ang.txt', 'rightf36_ang.txt', 'rightf3_par.txt', 
-#               'rightf4_par.txt', 'rightf5_par.txt', 'rightf6_par.txt', 'rightf7_par.txt', 'rightf8_par.txt', 'rightf9_par.txt']
+data_files = ['rightf10_par.txt', 'rightf11_par.txt', 'rightf12_par.txt', 'rightf13_per.txt', 'rightf14_per.txt', 
+              'rightf15_per.txt', 'rightf16_per.txt', 'rightf17_per.txt', 'rightf18_per.txt', 'rightf19_per.txt', 
+              'rightf1_par.txt', 'rightf20_per.txt', 'rightf21_per.txt', 'rightf22_per.txt', 'rightf23_per.txt', 
+              'rightf24_per.txt', 'rightf25_ang.txt', 'rightf26_ang.txt', 'rightf27_ang.txt', 'rightf28_ang.txt', 
+              'rightf29_ang.txt', 'rightf2_par.txt', 'rightf30_ang.txt', 'rightf31_ang.txt', 'rightf32_ang.txt', 
+              'rightf33_ang.txt', 'rightf34_ang.txt', 'rightf35_ang.txt', 'rightf36_ang.txt', 'rightf3_par.txt', 
+              'rightf4_par.txt', 'rightf5_par.txt', 'rightf6_par.txt', 'rightf7_par.txt', 'rightf8_par.txt', 'rightf9_par.txt']
 
-# main("Data\Data (02.21)\Right Foot", False)
+main("Data\Data (02.21)\Right Foot", False, 'foot')
 
-## ----------------------------------------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------------------------------------
 
 # Left Hand
-# base_files = ['baselh10.txt', 'baselh11.txt', 'baselh12.txt', 'baselh13.txt', 'baselh14.txt', 
-#               'baselh15.txt', 'baselh16.txt', 'baselh17.txt', 'baselh18.txt', 'baselh19.txt', 
-#               'baselh1.txt', 'baselh20.txt', 'baselh21.txt', 'baselh22.txt', 'baselh23.txt', 
-#               'baselh24.txt', 'baselh25.txt', 'baselh26.txt', 'baselh27.txt', 'baselh28.txt', 
-#               'baselh29.txt', 'baselh2.txt', 'baselh30.txt', 'baselh31.txt', 'baselh32.txt', 
-#               'baselh33.txt', 'baselh34.txt', 'baselh35.txt', 'baselh36.txt', 'baselh3.txt', 
-#               'baselh4.txt', 'baselh5.txt', 'baselh6.txt', 'baselh7.txt', 'baselh8.txt', 'baselh9.txt']
+base_files = ['baselh10.txt', 'baselh11.txt', 'baselh12.txt', 'baselh13.txt', 'baselh14.txt', 
+              'baselh15.txt', 'baselh16.txt', 'baselh17.txt', 'baselh18.txt', 'baselh19.txt', 
+              'baselh1.txt', 'baselh20.txt', 'baselh21.txt', 'baselh22.txt', 'baselh23.txt', 
+              'baselh24.txt', 'baselh25.txt', 'baselh26.txt', 'baselh27.txt', 'baselh28.txt', 
+              'baselh29.txt', 'baselh2.txt', 'baselh30.txt', 'baselh31.txt', 'baselh32.txt', 
+              'baselh33.txt', 'baselh34.txt', 'baselh35.txt', 'baselh36.txt', 'baselh3.txt', 
+              'baselh4.txt', 'baselh5.txt', 'baselh6.txt', 'baselh7.txt', 'baselh8.txt', 'baselh9.txt']
 
-# data_files = ['lefth10_per.txt', 'lefth11_per.txt', 'lefth12_per.txt', 'lefth13_par.txt', 'lefth14_par.txt', 
-#               'lefth15_par.txt', 'lefth16_par.txt', 'lefth17_par.txt', 'lefth18_par.txt', 'lefth19_par.txt', 
-#               'lefth1_per.txt', 'lefth20_par.txt', 'lefth21_par.txt', 'lefth22_par.txt', 'lefth23_par.txt', 
-#               'lefth24_par.txt', 'lefth25_ang.txt', 'lefth26_ang.txt', 'lefth27_ang.txt', 'lefth28_ang.txt', 
-#               'lefth29_ang.txt', 'lefth2_per.txt', 'lefth30_ang.txt', 'lefth31_ang.txt', 'lefth32_ang.txt', 
-#               'lefth33_ang.txt', 'lefth34_ang.txt', 'lefth35_ang.txt', 'lefth36_ang.txt', 'lefth3_per.txt', 
-#               'lefth4_per.txt', 'lefth5_per.txt', 'lefth6_per.txt', 'lefth7_per.txt', 'lefth8_per.txt', 'lefth9_per.txt']
+data_files = ['lefth10_per.txt', 'lefth11_per.txt', 'lefth12_per.txt', 'lefth13_par.txt', 'lefth14_par.txt', 
+              'lefth15_par.txt', 'lefth16_par.txt', 'lefth17_par.txt', 'lefth18_par.txt', 'lefth19_par.txt', 
+              'lefth1_per.txt', 'lefth20_par.txt', 'lefth21_par.txt', 'lefth22_par.txt', 'lefth23_par.txt', 
+              'lefth24_par.txt', 'lefth25_ang.txt', 'lefth26_ang.txt', 'lefth27_ang.txt', 'lefth28_ang.txt', 
+              'lefth29_ang.txt', 'lefth2_per.txt', 'lefth30_ang.txt', 'lefth31_ang.txt', 'lefth32_ang.txt', 
+              'lefth33_ang.txt', 'lefth34_ang.txt', 'lefth35_ang.txt', 'lefth36_ang.txt', 'lefth3_per.txt', 
+              'lefth4_per.txt', 'lefth5_per.txt', 'lefth6_per.txt', 'lefth7_per.txt', 'lefth8_per.txt', 'lefth9_per.txt']
 
-# main("Data\Data (02.21)\Left Hand", False)
+main("Data\Data (02.21)\Left Hand", False, 'hand')
 
-## ----------------------------------------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------------------------------------
 
 # Right Hand
 base_files = ['base10.txt', 'base11.txt', 'base12.txt', 'base13.txt', 'base14.txt', 
@@ -124,6 +125,6 @@ data_files = ['righth10_per.txt', 'righth11_per.txt', 'righth12_per.txt', 'right
               'righth3_per.txt', 'righth4_per.txt', 'righth5_per.txt', 'righth6_per.txt', 'righth7_per.txt', 
               'righth8_per.txt', 'righth9_per.txt']
 
-main("Data\Data (02.21)\Right Hand", False)
+main("Data\Data (02.21)\Right Hand", False, 'hand')
 
 
--- ImageProcessing/process_data.py
+++ ImageProcessing/process_data.py
@@ -3,13 +3,14 @@ import csv
 # https://theailearner.com/2020/11/03/opencv-minimum-area-rectangle/
 # Clarification of angle_of_rot
 class Bounding_Box:
-    def __init__(self, filepath, rect) -> None:
+    def __init__(self, filepath, rect, label) -> None:
         self.filepath = filepath
         self.centroid_x = rect[0][0]
         self.centroid_y = rect[0][1]
         self.width = rect[1][0]
         self.height = rect[1][1]
         self.rotation = rect[2]
+        self.label = label
 
     def set_label(self, label) -> None:
         self.label = label
@@ -22,7 +23,7 @@ def create_data_csv(data_list: list[Bounding_Box], filename):
         with open(filename, "a", newline='') as f:
             writer = csv.writer(f)
             for d in data_list:
-                writer.writerow([d.centroid_x, d.centroid_y, d.height, d.width, d.rotation, d.filepath])
+                writer.writerow([d.centroid_x, d.centroid_y, d.height, d.width, d.rotation, d.label])
     except BaseException as e:
         print("BaseException: ", filename)
     else:
