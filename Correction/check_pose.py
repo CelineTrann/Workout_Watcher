fd9c656cc0362bfda811c2aeaccf99a102a95e45
Celine Tran <celinephoungtran@gmail.com>
2023-02-25 18:26:46 -0500

    update angle checking

--- Correction/check_pose.py
+++ Correction/check_pose.py
@@ -4,9 +4,17 @@ import correcton_helper as cp
 def check_tree(data: dict[list]) -> bool:
     if not cp.check_distance(data['foot'], 10, 10, 5):
         return False
+    
+    elif not cp.check_angle(data['foot'], 0, 0, 5):
+        return False
+    
+    return True
 
 def check_warrior1(data) -> bool:
     if not cp.check_distance(data['foot'], 10, 10, 5):
+        return False
+    
+    elif not cp.check_angle(data['foot'], 0, 45, 5):
         return False
 
 def check_downwardDog(data) -> bool:
@@ -14,7 +22,16 @@ def check_downwardDog(data) -> bool:
         return False
     elif not cp.check_distance(data['hand'], 10, 10, 5):
         return False
+    
+    elif not cp.check_angle(data['foot'], 0, 0, 5):
+        return False
+    elif not cp.check_angle(data['hand'], 0, 0, 5):
+        return False
 
 def check_triangle(data) -> bool:
     if not cp.check_distance(data['foot'], 10, 10, 5):
+        return False
+    
+    # TODO: determine way to get 90 degrees
+    elif not cp.check_angle(data['foot'], 0, 0, 5):
         return False
