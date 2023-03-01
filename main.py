07b3bee5aaf8910ba19af8846f9f001869fb9b95
Celine Tran <celinephoungtran@gmail.com>
2023-03-01 09:36:47 -0500

    complete main function

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+from object_detection import process_img
 import Correction.check_pose as check
 import Correction.correct_pose as correct
+import readData as r
 
-from object_detection import process_img
-
+import time
 import pickle
 
 def check_pose(pose, data):
@@ -25,37 +26,45 @@ def correct_pose(pose):
 
     return switch.get(pose)
 
+def choose_pose():
+    # bluetooth to get pose
+    pose = 'tree'
+    return pose
+
 def main():
     # Load ML model
     with open("Model\or3_kneighbour.pkl", 'rb') as file:
         model = pickle.load(file)
 
-    # While On:
+    while True:
         # Wait for User to Choose Pose
-        pose = "tree"
+        pose = choose_pose()
+        if pose == 'exit':
+            break
 
-        holding_pose = True
-        while holding_pose:
-            # Get data from mat
-            base = 0
-            data = 0
+        start_pose_time = time.time()
+        while time.time() - start_pose_time < 30:
+            # Read data from mat
+            base = r.readData()
+            time.sleep(2)
+            data = r.readData()
             
-            # process and clean data
-            # process data image and get data from object
-            # classify object
-            # compile data (object type, location, rotation, pressure regions) 
+            # Extract Data
             object_list = process_img(base, data, model)
 
             if not object_list.is_valid():
                 break
 
+            # Check and correct pose
             if not check_pose(pose, object_list):
                 correct_pose(pose)
+                time.sleep(5)
+            else:
+                print("Hold Pose.")
+                time.sleep(10)
 
+        
 
-        # if not continue:
-            # send result to phone
-            # break
         
 
 
