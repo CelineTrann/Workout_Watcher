45bcb08afb4a309c5efc5a8162c77d458ea4e3af
Celine Tran <celinephoungtran@gmail.com>
2023-03-03 20:59:07 -0500

    Fix how check and correct functions are called

--- main.py
+++ main.py
@@ -6,30 +6,37 @@ import readData as r
 
 import time
 
-def check_pose(pose, data):
-    switch = {
-        "tree_right": check.check_tree(data, side.RIGHT),
-        "tree_left": check.check_tree(data, side.LEFT),
-        "warrior1_right": check.check_warrior1(data, 0, 45),
-        "warrior1_left": check.check_warrior1(data, 45, 0),
-        "downwardDog": check.check_downwardDog(data),
-        "triangle_right": check.check_triangle(data, 0, 90),
-        "triangle_left": check.check_triangle(data, 90, 0)
-    }
+def check_pose(pose, data) -> bool:
+    if pose == "tree_right": 
+        check.check_tree(data, side.RIGHT)
+    elif pose == "tree_left": 
+        check.check_tree(data, side.LEFT)
+    elif pose ==  "warrior1_right": 
+        check.check_warrior1(data, 0, 45)
+    elif pose == "warrior1_left": 
+        return check.check_warrior1(data, 45, 0)
+    elif pose == "downwardDog": 
+        return  check.check_downwardDog(data)
+    elif pose == "triangle_right": 
+        return  check.check_triangle(data, 0, 90)
+    elif pose ==  "triangle_left": 
+        return  check.check_triangle(data, 90, 0)
+ 
+    return False
 
-    return switch.get(pose)
-
-def correct_pose(pose, data):
-    switch = {
-        "tree": correct.correct_tree(data),
-        "warrior1_right": correct.correct_warrior1(data, 0, 45),
-        "warrior1_left": correct.correct_warrior1(data, 45, 0),
-        "downwardDog": correct.correct_downwardDog(data),
-        "triangle_right": correct.correct_triangle(data, 0, 90),
-        "triangle_left": correct.correct_triangle(data, 90, 0)
-    }
-
-    return switch.get(pose)
+def correct_pose(pose, data) -> None:
+    if pose == "tree": 
+        correct.correct_tree(data)
+    elif pose == "warrior1_right": 
+        correct.correct_warrior1(data, 0, 45)
+    elif pose == "warrior1_left": 
+        correct.correct_warrior1(data, 45, 0)
+    elif pose == "downwardDog": 
+        correct.correct_downwardDog(data)
+    elif pose == "triangle_right": 
+        correct.correct_triangle(data, 0, 90)
+    elif pose == "triangle_left": 
+        correct.correct_triangle(data, 90, 0)
 
 def choose_pose():
     # bluetooth to get pose
